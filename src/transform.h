691c4b77283f5e6fa6001913f7ff046a8b1d16ed
Jonas Kulla <Nyocurio@gmail.com>
2013-09-01 18:26:36 +0200

    Add GPL license header

--- src/transform.h
+++ src/transform.h
@@ -1,8 +1,23 @@
 /*
- *  transform.h
- *
- *  This file is part of mkxp. It is based on parts of "SFML 2.0"
- */
+** tilequad.h
+**
+** This file is part of mkxp. It is based on parts of "SFML 2.0" (license further below)
+**
+** Copyright (C) 2013 Jonas Kulla <Nyocurio@gmail.com>
+**
+** mkxp is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 2 of the License, or
+** (at your option) any later version.
+**
+** mkxp is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with mkxp.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 ////////////////////////////////////////////////////////////
 //
