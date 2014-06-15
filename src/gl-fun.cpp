a4615fb8ccddd75494a047b27df7134dc8be7ad7
Jonas Kulla <Nyocurio@gmail.com>
2014-06-15 07:52:26 +0200

    Add copyright notice to gl-fun.{cpp,h}

--- src/gl-fun.cpp
+++ src/gl-fun.cpp
@@ -1,3 +1,24 @@
+/*
+** gl-fun.cpp
+**
+** This file is part of mkxp.
+**
+** Copyright (C) 2014 Jonas Kulla <Nyocurio@gmail.com>
+**
+** mkxp is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 2 of the License, or
+** (at your option) any later version.
+**
+** mkxp is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with mkxp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "gl-fun.h"
 
 #include "boost-hash.h"
