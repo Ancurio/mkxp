d439b1d833ed4cdc49b25d400cfa806f4f143e0a
Jonas Kulla <Nyocurio@gmail.com>
2014-08-16 01:25:42 +0200

    MRI-Binding: Add RGSS2 RPG module

--- binding-mri/module_rpg.cpp
+++ binding-mri/module_rpg.cpp
@@ -1,6 +1,8 @@
 
 #ifdef RGSS3
-#include "module_rpg3.rb.xxd"
+# include "module_rpg3.rb.xxd"
+#elif RGSS2
+# include "module_rpg2.rb.xxd"
 #else
-#include "module_rpg.rb.xxd"
+# include "module_rpg.rb.xxd"
 #endif
