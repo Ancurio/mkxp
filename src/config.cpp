9a3dd2d554df7255b592bba00d5cfea065af475b
Jonas Kulla <Nyocurio@gmail.com>
2015-07-16 18:41:48 +0200

    Config: Enable smoothScaling by default
    There's really no reason not to.

--- src/config.cpp
+++ src/config.cpp
@@ -151,7 +151,7 @@ void Config::read(int argc, char *argv[])
 	PO_DESC(winResizable, bool, false) \
 	PO_DESC(fullscreen, bool, false) \
 	PO_DESC(fixedAspectRatio, bool, true) \
-	PO_DESC(smoothScaling, bool, false) \
+	PO_DESC(smoothScaling, bool, true) \
 	PO_DESC(vsync, bool, false) \
 	PO_DESC(defScreenW, int, 0) \
 	PO_DESC(defScreenH, int, 0) \
