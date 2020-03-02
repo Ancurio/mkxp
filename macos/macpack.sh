f859c69b02d0582554381c933c9f663c2e1508e3
Struma <strix@struma.me>
2020-03-02 05:01:42 -0500

    fixup macOS install script

--- macos/macpack.sh
+++ macos/macpack.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-EXE=${MESON_INSTALL_PREFIX}/Contents/MacOS/mkxp-z
-install_name_tool -add_rpath "@executable_path/../libs" $EXE
-
-macpack $EXE
+EXE=${MESON_INSTALL_PREFIX}/Contents/MacOS/$2
 
 if [ -n "$1" ]; then
   echo "Setting up steam_api manually..."
+  mkdir -p "${MESON_INSTALL_PREFIX}/Contents/libs"
   cp "$1/libsteam_api.dylib" "${MESON_INSTALL_PREFIX}/Contents/libs"
   install_name_tool -change "@loader_path/libsteam_api.dylib" "@executable_path/../libs/libsteam_api.dylib" $EXE
+  install_name_tool -add_rpath "@executable_path/../libs" ${EXE}_child
+  macpack ${EXE}_child
+else
+  install_name_tool -add_rpath "@executable_path/../libs" ${EXE}
+  macpack ${EXE}
 fi
