fff60b21da7bc8d58a8b15af8a5b382c97dec6c5
Struma <strix@struma.me>
2020-11-16 07:08:07 -0500

    Request timestamp if codesign identity is provided

--- macos/codesign.sh
+++ macos/codesign.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 IDENTITY="$1"
+TIMESTAMP=--timestamp
 if [[ $IDENTITY == "" ]]; then
     IDENTITY=-
+    TIMESTAMP=""
 fi
 
-  codesign -vfs $IDENTITY --entitlements "${MESON_SOURCE_ROOT}/macos/entitlements.plist" --deep -o runtime "${MESON_INSTALL_PREFIX}"
+  codesign -vfs $IDENTITY --entitlements "${MESON_SOURCE_ROOT}/macos/entitlements.plist" --deep --ignore-resources -o runtime $TIMESTAMP "${MESON_INSTALL_PREFIX}"
