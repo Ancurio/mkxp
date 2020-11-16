fe47cc91e511355b76a92c945873e62c073a22a6
Struma <strix@struma.me>
2020-11-16 04:37:05 -0500

    Add macOS signing options, sandbox signed bundles

--- macos/macpack.sh
+++ macos/macpack.sh
@@ -4,12 +4,12 @@ EXE=${MESON_INSTALL_PREFIX}/Contents/MacOS/$2
 
 if [ -n "$1" ]; then
   echo "Setting up steam_api manually..."
-  mkdir -p "${MESON_INSTALL_PREFIX}/Contents/libs"
-  cp "$1/libsteam_api.dylib" "${MESON_INSTALL_PREFIX}/Contents/libs"
-  install_name_tool -change "@loader_path/libsteam_api.dylib" "@executable_path/../libs/libsteam_api.dylib" $EXE
-  install_name_tool -add_rpath "@executable_path/../libs" ${EXE}_rt
-  macpack ${EXE}_rt
+  mkdir -p "${MESON_INSTALL_PREFIX}/Contents/Frameworks"
+  cp "$1/libsteam_api.dylib" "${MESON_INSTALL_PREFIX}/Contents/Frameworks"
+  install_name_tool -change "@loader_path/libsteam_api.dylib" "@executable_path/../Frameworks/libsteam_api.dylib" $EXE
+  install_name_tool -add_rpath "@executable_path/../Frameworks" ${EXE}_rt
+  macpack -d "${MESON_INSTALL_PREFIX}/Contents/Frameworks" ${EXE}_rt
 else
-  install_name_tool -add_rpath "@executable_path/../libs" ${EXE}
-  macpack ${EXE}
+  install_name_tool -add_rpath "@executable_path/../Frameworks" ${EXE}
+  macpack -d "${MESON_INSTALL_PREFIX}/Contents/Frameworks" ${EXE}
 fi
