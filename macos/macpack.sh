a9d7e0783af6c94a03017538d23716926b43b64d
Struma <strix@struma.me>
2020-11-16 20:26:49 -0500

    Preliminary attempt at Metal support

--- macos/macpack.sh
+++ macos/macpack.sh
@@ -2,10 +2,22 @@
 
 EXE=${MESON_INSTALL_PREFIX}/Contents/MacOS/$2
 
+ANGLE="${MESON_SOURCE_ROOT}/macos/lib/MetalANGLE.framework"
+FRAMEWORKS="${MESON_INSTALL_PREFIX}/Contents/Frameworks"
+
+if [ -n "$(otool -L $EXE | grep ANGLE)" ] && [ -d $ANGLE ]; then
+  if [ ! -d $FRAMEWORKS ]; then
+    mkdir -p $FRAMEWORKS
+  fi
+  cp -a $ANGLE $FRAMEWORKS
+fi
+
 if [ -n "$1" ]; then
   echo "Setting up steam_api manually..."
-  mkdir -p "${MESON_INSTALL_PREFIX}/Contents/Frameworks"
-  cp "$1/libsteam_api.dylib" "${MESON_INSTALL_PREFIX}/Contents/Frameworks"
+  if [ ! -d $FRAMEWORKS ]; then
+    mkdir -p $FRAMEWORKS
+  fi
+  cp "$1/libsteam_api.dylib" $FRAMEWORKS
   install_name_tool -change "@loader_path/libsteam_api.dylib" "@executable_path/../Frameworks/libsteam_api.dylib" $EXE
   install_name_tool -add_rpath "@executable_path/../Frameworks" ${EXE}_rt
   macpack -d "../Frameworks" ${EXE}_rt
