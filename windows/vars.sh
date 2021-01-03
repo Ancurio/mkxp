a05f8a6d10fc5d1a67231313423b1b8c83e4a6eb
Struma <strix@struma.me>
2021-01-03 05:06:55 -0500

    set cwd properly when running vars.sh

--- windows/vars.sh
+++ windows/vars.sh
@@ -1,8 +1,10 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 MKXPZ_HOST=$(gcc -dumpmachine)
 MKXPZ_PREFIX=$(ruby -e "printf ('${MKXPZ_HOST}'[/i686/].nil?) ? 'mingw64' : 'mingw'")
-export LDFLAGS="-L$PWD/build-${MKXPZ_PREFIX}/lib -L$PWD/build-${MKXPZ_PREFIX}/bin"
-export PATH="$PWD/build-${MKXPZ_PREFIX}/bin:$PATH"
+export LDFLAGS="-L$DIR/build-${MKXPZ_PREFIX}/lib -L$DIR/build-${MKXPZ_PREFIX}/bin"
+export PATH="$DIR/build-${MKXPZ_PREFIX}/bin:$PATH"
 MKXPZ_OLD_PC=$(pkg-config --variable pc_path pkg-config)
 
 # Try to load the stuff we built first
-export PKG_CONFIG_PATH="$PWD/build-${MKXPZ_PREFIX}/lib/pkgconfig"
+export PKG_CONFIG_PATH="$DIR/build-${MKXPZ_PREFIX}/lib/pkgconfig"
