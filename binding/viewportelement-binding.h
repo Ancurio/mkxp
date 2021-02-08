6a006f1b13ed7ab059f5bf105a1b0afcf442a3f2
Struma <strix@struma.me>
2021-02-08 03:18:17 -0500

    Remove RGSS version check from viewportElementBinding

--- binding/viewportelement-binding.h
+++ binding/viewportelement-binding.h
@@ -97,10 +97,10 @@ viewportElementBindingInit(VALUE klass)
 
 	_rb_define_method(klass, "viewport", viewportElementGetViewport<C>);
 
-	if (rgssVer >= 2)
-	{
+    //if (rgssVer >= 2)
+	//{
 	_rb_define_method(klass, "viewport=", viewportElementSetViewport<C>);
-	}
+	//}
 }
 
 #endif // VIEWPORTELEMENTBINDING_H
