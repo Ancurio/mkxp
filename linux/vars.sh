ad0375437840a367d928ac18f8d0585d79bc7467
Struma <strix@struma.me>
2021-02-21 22:23:31 -0500

    link linux binary (mostly) statically

--- linux/vars.sh
+++ linux/vars.sh
@@ -1,11 +1,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-MKXPZ_PREFIX=$(shell gcc -dumpmachine | sed -r "s/(.+)-.+-.+/\1/")
+MKXPZ_PREFIX=$(gcc -dumpmachine | sed -r "s/(.+)-.+-.+/\1/")
 export LDFLAGS="-L$DIR/build-${MKXPZ_PREFIX}/lib -L$DIR/build-${MKXPZ_PREFIX}/bin"
 MKXPZ_OLD_PC=$(pkg-config --variable pc_path pkg-config)
 
 # Try to load the stuff we built first
-export PKG_CONFIG_LIBDIR="$DIR/build-${MKXPZ_PREFIX}/lib/pkgconfig:${MKXPZ_OLD_PC}"
+export PKG_CONFIG_LIBDIR="$DIR/build-${MKXPZ_PREFIX}/lib/pkgconfig:$DIR/build-${MKXPZ_PREFIX}/lib64/pkgconfig:${MKXPZ_OLD_PC}"
 export PATH="$DIR/build-${MKXPZ_PREFIX}/bin:$PATH"
 export LD_LIBRARY_PATH="$DIR/build-${MKXPZ_PREFIX}/lib:${LD_LIBRARY_PATH}"
 export MKXPZ_PREFIX="$DIR/build-${MKXPZ_PREFIX}"
