e75c8747e126442fe151eff1c7eb311d0124adc5
Struma <strix@struma.me>
2021-02-24 00:28:54 -0500

    Update SDL_sound to 2.0

--- linux/vars.sh
+++ linux/vars.sh
@@ -1,7 +1,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 MKXPZ_PREFIX=$(gcc -dumpmachine | sed -r "s/(.+)-.+-.+/\1/")
-export LDFLAGS="-L$DIR/build-${MKXPZ_PREFIX}/lib -L$DIR/build-${MKXPZ_PREFIX}/bin"
+export LDFLAGS="-L$DIR/build-${MKXPZ_PREFIX}/lib"
+export CFLAGS="-I$DIR/build-${MKXPZ_PREFIX}/include"
 MKXPZ_OLD_PC=$(pkg-config --variable pc_path pkg-config)
 
 # Try to load the stuff we built first
--- windows/vars.sh
+++ windows/vars.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 MKXPZ_HOST=$(gcc -dumpmachine)
 MKXPZ_PREFIX=$(ruby -e "printf ('${MKXPZ_HOST}'[/i686/].nil?) ? 'mingw64' : 'mingw'")
 export LDFLAGS="-L$DIR/build-${MKXPZ_PREFIX}/lib -L$DIR/build-${MKXPZ_PREFIX}/bin"
+export CFLAGS="-I$DIR/build-${MKXPZ_PREFIX}/include"
 export PATH="$DIR/build-${MKXPZ_PREFIX}/bin:$PATH"
 MKXPZ_OLD_PC=$(pkg-config --variable pc_path pkg-config)
 
