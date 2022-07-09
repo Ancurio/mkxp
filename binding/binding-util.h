bd075d62fe156843d2c4aab2e70ff5cda54320b4
Snowdream <strumajen@icloud.com>
2022-07-09 19:28:23 -0400

    Older versions of Ruby 2 don't have rb_utf8_str_*

--- binding/binding-util.h
+++ binding/binding-util.h
@@ -168,14 +168,15 @@ return Data_Wrap_Struct(klass, 0, free, 0);                                \
 
 #define DEF_ALLOCFUNC(type) DEF_ALLOCFUNC_CUSTOMFREE(type, freeInstance<type>)
 
-#if RAPI_MINOR < 9
+#define PRIsVALUE "s"
+
+#endif
+
+#if RAPI_FULL < 240
 #define rb_utf8_str_new_cstr rb_str_new2
 #define rb_utf8_str_new rb_str_new
 #endif
 
-#define PRIsVALUE "s"
-
-#endif
 // end
 
 #if RAPI_FULL > 187
